9d8a992ab8ead3d0965ec94e565945f517434f58
JeffRisberg <j.s.risberg@gmail.com>
2020-12-27 17:30:04 -0800

    added sentiment analysis and pickle file

--- part4/chloes_flask_demo/web/app.py
+++ part4/chloes_flask_demo/web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from predict_sentiment_analysis import get_sentiment
 
 app = Flask(__name__)
 
@@ -6,5 +7,15 @@ app = Flask(__name__)
 def hello_whale():
     return render_template("whale_hello.html")
 
+@app.route('/predict', methods = ['GET', 'POST'])
+def predict():
+    if request.method == 'GET':
+        input = request.args.get('input')
+    else:
+        input = request.get_json(force=True)['input']
+    if not input:
+        return 'No input value found'
+    return get_sentiment(input)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, use_reloader=False, host='0.0.0.0')
